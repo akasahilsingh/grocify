ff8780361271d55dcfce15f494f057837ef966d8
Sahil Singh <akasahilsingh@gmail.com>
2025-09-05 16:24:11 +0530

    feat: Added hero section and refactor the code for mobile responsiveness

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <section>
+        <div className='py-20'>
+<div className='w-fit mx-auto'>
+  <h2 className='text-5xl font-bold'><span className='text-orange-500'>Shop</span> by Category</h2>
+    <div className='w-34 h-1 bg-orange-500 mt-6 ml-auto'>
+
+    </div>
+
+    </div>
+        </div>
+        </section>
+  )
+}
+
+export default Category
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,126 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaHeart } from "react-icons/fa6";
+import { RiShoppingBag4Fill } from "react-icons/ri";
+import { FaSearch } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
+import { TbMenu3 } from "react-icons/tb";
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
   return (
-    <header className='bg-white'>
-        <nav className='max-w-[1400px] mx-auto h-[14vh] px-10 items-center flex justify-between'>
-            {/* Logo */}
-            <a href='#' className='text-3xl font-bold'>
-                Gr<span className='text-orange-500'>O</span>cify
+    <header className="bg-white fixed top-0 left-0 right-0">
+      <nav className="max-w-[1400px] mx-auto md:h-[14vh] h-[12vh] px-10 items-center flex justify-between">
+        {/* Logo */}
+        <a href="#" className="text-3xl font-bold">
+          Gr<span className="text-orange-500">O</span>cify
+        </a>
+
+        {/* Desktop Menu */}
+        <ul className="md:flex items-center gap-x-15" hidden>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-orange-500"
+            >
+              Home
             </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
 
-            {/* Desktop Menu */}
-            <ul className='flex items-center gap-x-15'>
-            <li>
-                <a href='#' className='font-semibold tracking-wider text-orange-500'>Home</a>
+        {/* Nav Action */}
+        <div className="flex items-center gap-x-5">
+            {/* Input field */}
+            <div className="md:flex bg-red-100 p-1 border-2 border-orange-500 rounded-full hidden">
+                <input type="text" name="text" id="text" placeholder="Search..." autoComplete="off" 
+                className="flex-1 h-[5vh] px-3 focus:outline-none"/> 
+                <button className="text-white w-10 h-10 flex items-center justify-center rounded-full text-xl bg-gradient-to-b from-orange-400 to-orange-500"> 
+                    <FaSearch />
+                </button>
+            </div>
+          <a href="#" className="text-zinc-800 text-2xl">
+            <FaHeart />
+          </a>
+          <a href="#" className="text-zinc-800 text-2xl">
+            <RiShoppingBag4Fill />
+          </a>
+          {/* Hamburger */}
+          <a href="#" className="text-3xl text-zinc-800 md:hidden" onClick={toggleMenu}>
+            {showMenu ? <TbMenu3 /> : <IoMdMenu />}
+          </a>
+        </div>
+        {/* Mobile menu */}
+        <ul className={`flex flex-col gap-y-12 bg-orange-500/15 backdrop-blur-xl p-10 rounded-xl items-center gap-x-15 md:hidden absolute top-30 -left-full tranform -translate-x-1/2 transition-all duration-300 ${showMenu ? 'left-1/2' : ""}`}>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-orange-500"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Contact Us
+            </a>
+          </li>
+          <li className="flex bg-red-100 p-1 border-2 border-orange-500 rounded-full md:hidden">
+                <input type="text" name="text" id="text" placeholder="Search..." autoComplete="off" 
+                className="flex-1 h-[5vh] px-3 focus:outline-none"/> 
+                <button className="text-white w-10 h-10 flex items-center justify-center rounded-full text-xl bg-gradient-to-b from-orange-400 to-orange-500"> 
+                    <FaSearch />
+                </button>
             </li>
-            <li>
-                <a href='#' className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>About Us</a>
-            </li>
-            <li>
-                <a href='#' className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Process</a>
-            </li>
-            <li>
-                <a href='#' className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Contact Us</a>
-            </li>
-            </ul>
-
-            {/* Nav Action */}
-        </nav>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
